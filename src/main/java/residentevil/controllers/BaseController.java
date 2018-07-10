22114c27b87d86e75cda3aaf63fbbdb7caf7d705
Radoslav Evgeniev <r.evgeniev.ivanov@gmail.com>
2018-07-10 17:06:24 +0300

    Initial commit

---
+++ src/main/java/residentevil/controllers/BaseController.java
@@ -0,0 +1,24 @@
+package residentevil.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public class BaseController {
+
+    protected ModelAndView view(String view, ModelAndView modelAndView) {
+        modelAndView.setViewName(view);
+
+        return modelAndView;
+    }
+
+    protected ModelAndView view(String view) {
+        return this.view(view, new ModelAndView());
+    }
+
+    protected ModelAndView redirect(String url) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("redirect:" + url);
+
+        return modelAndView;
+    }
+}
---
+++ src/main/java/residentevil/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package residentevil.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController extends BaseController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return super.view("index");
+    }
+}
